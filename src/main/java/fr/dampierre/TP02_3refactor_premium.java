525ce77ea0a6045cac03ec3cb369cd31e5e44a1c
SauroMatheo <sauromatheo@gmail.com>
2022-09-29 08:43:50 +0200

    Renommage des fichiers pour le num exo

---
+++ src/main/java/fr/dampierre/TP02_3refactor_premium.java
@@ -0,0 +1,34 @@
+package fr.dampierre;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class TP02_3refactor_premium {
+    static int compteBillets(int montant) {
+        System.out.println();
+        int nbBillets = montant / 20;
+        if (nbBillets * 20 < montant) {
+            nbBillets++;
+        }
+        return nbBillets;
+    }
+
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+
+        String[] prenoms = { "Fred", "Cathy" };
+        List<Integer> billets = new ArrayList<Integer>();
+
+        for (int i = 0; i < prenoms.length; i++) {
+            System.out.print("Combien va dépenser " + prenoms[i] + " ? ");
+            billets.add(compteBillets(console.nextInt()));
+        }
+
+        console.close();
+
+        for (int i = 0; i < prenoms.length; i++) {
+            System.out.println(prenoms[i] + " aura besoin de " + billets.get(i) + " billets de 20.");
+        }
+    }
+}
