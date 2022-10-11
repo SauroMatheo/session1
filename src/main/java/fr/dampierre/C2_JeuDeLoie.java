87af0e41ce87d27990478a3e389c13149fc92c07
SauroMatheo <sauromatheo@gmail.com>
2022-10-11 11:06:00 +0200

    Boucle de victoire

--- src/main/java/fr/dampierre/C2_JeuDeLoie.java
+++ src/main/java/fr/dampierre/C2_JeuDeLoie.java
@@ -5,24 +5,38 @@ import java.util.Random;
 public class C2_JeuDeLoie {
     public static void main(String[] args) {
         Random generateur = new Random();
-        int progression = 0;
         int nbLancers = 5;
         int nbCases = 20; // Sans compter start
 
+        int progression = 0;
+        boolean victoire = false;
+        int parties = 0;
+
         int i = 0;
         int aleatoire;
-        while (i < nbLancers && progression < 20) {
-            aleatoire = generateur.nextInt(6) + 1;
-            progression += aleatoire;
-            i++;
-            System.out.println("Vous avez roulé un " + aleatoire + ".");
+
+        while (!victoire) {
+            i = 0;
+            progression = 0;
+
+            parties++;
+
+            while (i < nbLancers && progression < 20) {
+                aleatoire = generateur.nextInt(6) + 1;
+                progression += aleatoire;
+                i++;
+                System.out.println("Vous avez roulé un " + aleatoire);
+                System.out.println("Vous êtes à la case " + progression);
+            }
+
+            victoire = nbCases == progression;
+            if (!victoire) {
+                System.out.println(
+                        "Vous avez perdu ! Vous avez traversé " + progression + " cases en " + i + " lancers.");
+            }
         }
 
-        if (nbCases == progression) {
-            System.out.println(
-                    "Vous avez gagné ! Vous avez traversé les " + progression + " cases en " + i + " lancers.");
-        } else {
-            System.out.println("Vous avez perdu ! Vous avez traversé " + progression + " cases en " + i + " lancers.");
-        }
+        System.out.println("Vous avez gagné ! Vous avez traversé les " + progression + " cases en " + i
+                + " lancers et en " + parties + " parties !");
     }
 }
