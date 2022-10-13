49d22deb40d32f1676ff2f2219d63b99133d74ac
SauroMatheo <sauromatheo@gmail.com>
2022-10-13 09:57:39 +0200

    TP02 XO 13-14

---
+++ src/main/java/fr/dampierre/TP02/XO13_14.java
@@ -0,0 +1,55 @@
+package fr.dampierre.TP02;
+
+public class XO13_14 {
+    static boolean deuxSens(String text) {
+        String reversed = new StringBuffer(text).reverse().toString();
+        return text.equalsIgnoreCase(reversed);
+    }
+
+    static boolean deuxSensComplexe(String text) {
+        text = text.toLowerCase();
+        int len = text.length();
+        for (int i = 0; i < len; i++) {
+            if (Character.compare(text.charAt(i), text.charAt(len - i - 1)) != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    static String retirePonctuation(String text) {
+        String compact = "";
+
+        for (int i = 0; i < text.length(); i++) {
+            char chara = text.charAt(i);
+            if (Character.isLetter(chara)) {
+                compact += chara;
+            }
+        }
+        return compact;
+    }
+
+    static boolean deuxSensClement(String text) {
+        text = retirePonctuation(text);
+        String reversed = new StringBuffer(text).reverse().toString();
+
+        return text.equalsIgnoreCase(reversed);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("NORMAL");
+        System.out.println(deuxSens("quelconque")); // affichera 'false' car "quelconque" pas pareil que "euqnocleuq"
+        System.out.println(deuxSens("kayak")); // 'true'
+        System.out.println(deuxSens("Un radar NU")); // 'true' (insensible à la casse)
+        System.out.println(deuxSens("Éric notre valet alla te laver ton ciré")); // 'false' (à cause des espaces et de
+                                                                                 // la ponctuation)
+        // Ce dernier exemple fonctionne aussi avec "Luc" à la place de "Eric"
+        System.out.println("===\nSANS ESPACES");
+        System.out.println(deuxSensClement("quelconque")); // affichera 'false' car "quelconque" pas pareil que
+                                                           // "euqnocleuq"
+        System.out.println(deuxSensClement("kayak")); // 'true'
+        System.out.println(deuxSensClement("Un radar NU")); // 'true' (insensible à la casse)
+        System.out.println(deuxSensClement("Éric notre valet alla te laver ton ciré"));
+        // Ce dernier exemple fonctionne aussi avec "Luc" à la place de "Eric"
+    }
+}
