06fcc3683a4b60c20702d7b4ed88e2db94875ebf
SauroMatheo <sauromatheo@gmail.com>
2022-10-13 08:28:16 +0200

    TP03 XO1

---
+++ src/main/java/fr/dampierre/TP03/XO1_suites.java
@@ -0,0 +1,17 @@
+package fr.dampierre.TP03;
+
+public class XO1_suites {
+    public static void main(String[] args) {
+        // Suite 1
+        int suite1 = -4;
+        String sortie1 = "" + suite1;
+        for (int i = 0; i < 5; i++) {
+            suite1 += 18;
+            sortie1 += " " + suite1;
+        }
+        System.out.println(sortie1);
+
+        // Suite 2
+
+    }
+}
